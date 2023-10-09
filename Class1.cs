7c3210493f6cb1c64b307475fcff596c175402c1
Victor Jensen <Nexsysisa@gmail.com>
2023-10-09 21:02:42 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Carvromvroom
+{
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public string LicensePlate { get; set; }
+
+
+
+        public bool ValidateModel()
+        {
+            return !string.IsNullOrEmpty(Model) && Model.Length >= 4;
+        }
+
+
+        public bool ValidatePrice()
+        {
+            return Price > 0;
+        }
+
+
+        public bool ValidateLicensePlate()
+        {
+            int length = LicensePlate.Length;
+            return length >= 2 && length <= 7;
+        }
+
+
+    }
+}
---
+++ VROOOMRepo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Carvromvroom
+{
+    public class VROOOMRepo
+    {
+
+
+
+        private List<Car> carsgovroom = new List<Car>();
+
+
+        public IEnumerable<Car> GetAll()
+        {
+            return carsgovroom;
+        }
+
+
+
+
+        public Car GetBookByID(int id)
+        {
+            foreach (var book in carsgovroom)
+            {
+                if (book.Id == id) return book;
+            }
+            return null;
+        }
+
+
+
+        public Car Add(Car book)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException();
+            }
+            int newID = carsgovroom.Count > 0 ? carsgovroom.Max(b => b.Id) + 1 : 1;
+            book.Id = newID;
+
+            carsgovroom.Add(book);
+
+            return book;
+        }
+
+
+        public Car Delete(int id)
+        {
+            Car booktodelete = null;
+            foreach (var book in carsgovroom)
+            {
+                if (book.Id == id)
+                    booktodelete = book;
+                carsgovroom.Remove(booktodelete);
+            }
+            return booktodelete;
+        }
+
+
+        public bool Update(int id, Car UpdateBookie)
+        {
+            var carToUpdate = carsgovroom.FirstOrDefault(car => car.Id == id);
+            if (carToUpdate != null)
+            {
+                carToUpdate.Model = UpdateBookie.Model;
+                carToUpdate.Price = UpdateBookie.Price;
+                carToUpdate.LicensePlate = UpdateBookie.LicensePlate;
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
